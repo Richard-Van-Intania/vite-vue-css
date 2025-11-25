44e3832ffb7e70ded607a91f85b677590ab43d94
Ittipat Pattum <nampu@finema.co>
2025-11-26 00:29:00 +0700

    save

--- src/components/modals/PlainModal.vue
+++ src/components/modals/PlainModal.vue
@@ -5,10 +5,10 @@ const showDialog = ref<boolean>(false);
 
 <template>
     <div class="flex justify-end items-end min-h-lvh w-lvw">
-        <button type="button" class="bg-blue-500 px-8 py-4 rounded-full text-white font-medium text-2xl m-10 z-10" v-on:click="showDialog = !showDialog">Show dialog</button>
+        <button type="button" class="bg-blue-500 px-8 py-4 rounded-full text-white font-medium text-2xl m-10 shadow-xl z-10" v-on:click="showDialog = !showDialog">Show dialog</button>
     </div>
-    <div class="fixed inset-0 w-full h-full bg-blk-op-50 justify-center items-center z-50" v-bind:class="[showDialog ? 'flex' : 'hidden']">
-        <div class="h-36 w-72 bg-white">holaaa</div>
+    <div class="fixed inset-0 w-full h-full bg-blk-op-50 justify-center items-center z-40" v-bind:class="[showDialog ? 'flex' : 'hidden']">
+        <div class="h-36 w-72 bg-white z-50">holaaa</div>
     </div>
 </template>
 
