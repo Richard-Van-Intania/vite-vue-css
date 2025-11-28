ed2b5bb23238f60dcd7d15b2ce421bf8fe21dc5b
Ittipat Pattum <nampu@finema.co>
2025-11-28 23:40:43 +0700

    save

--- src/components/offcanvas/BasicOffcanvas.vue
+++ src/components/offcanvas/BasicOffcanvas.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 import { ref } from "vue";
 const show = ref<boolean>(false);
+
+function open(): void {
+  show.value = true;
+}
+
+function close(): void {
+  show.value = false;
+}
 </script>
 
 <template>
   <div class="fixed right-0 bottom-0 size-auto max-h-none max-w-none">
-    <button type="button" class="bg-blue-500 px-8 py-4 rounded-full text-white font-medium text-2xl cursor-pointer shadow-lg m-10 z-10" v-on:click="show = !show">Show Offcanvas</button>
+    <button type="button" class="bg-blue-500 px-8 py-4 rounded-full text-white font-medium text-2xl cursor-pointer shadow-lg m-10 z-10" v-on:click="open()">Show Offcanvas</button>
   </div>
   <div></div>
 </template>
