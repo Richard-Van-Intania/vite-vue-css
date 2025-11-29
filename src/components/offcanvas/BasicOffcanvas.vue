87c7c389c58f23d9f07ebd3821e5cecdde01e77b
Ittipat Pattum <nampu@finema.co>
2025-11-29 12:22:11 +0700

    save

--- src/components/offcanvas/BasicOffcanvas.vue
+++ src/components/offcanvas/BasicOffcanvas.vue
@@ -13,9 +13,10 @@ function close(): void {
 </script>
 
 <template>
-  <div>
+  <div id="btn">
     <button type="button" class="cursor-pointer my-4 mx-6" v-on:click="close()"><Icon icon="ri:menu-line" style="color: black; font-size: 32px" /></button>
   </div>
+  <div id="overlay" class="fixed inset-0 size-auto max-h-none max-w-none bg-blk-op-40 z-20"><div class=""></div></div>
 </template>
 
 <style scoped></style>
