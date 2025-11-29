95c43ccd7978e65010b0534a1299ee7b6c901585
Ittipat Pattum <nampu@finema.co>
2025-11-29 15:00:23 +0700

    save

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 import PlainModal from "./components/modals/PlainModal.vue";
 import BasicOffcanvas from "./components/offcanvas/BasicOffcanvas.vue";
 import OffcanvasVtran from "./components/offcanvas/OffcanvasVtran.vue";
+import BasicToast from "./components/toast/BasicToast.vue";
 </script>
 
 <template>
-  <OffcanvasVtran />
+  <BasicToast />
 </template>
 
 <style scoped></style>
---
+++ src/components/toast/BasicToast.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { Icon } from "@iconify/vue";
+
+const show = ref(false);
+
+function open() {
+  show.value = true;
+}
+
+function close() {
+  show.value = false;
+}
+</script>
+
+<template>
+  <div id="btn-open">
+    <button class="cursor-pointer m-10" @click="open">
+      <Icon icon="ri:notification-2-line" style="color: black; font-size: 32px" />
+    </button>
+  </div>
+  <div></div>
+</template>
+
+<style scoped></style>
