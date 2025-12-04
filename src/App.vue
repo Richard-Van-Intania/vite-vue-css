4696d85ebef181ee6bc1d6afdbcbe14bc4197341
Ittipat Pattum <nampu@finema.co>
2025-12-04 19:08:39 +0700

    save

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import BasicCarousel from "./components/carousel/BasicCarousel.vue";
 import BasicToast from "./components/toast/BasicToast.vue";
 import SepToast from "./components/toast/SepToast.vue";
 </script>
 
 <template>
-  <SepToast />
+  <BasicCarousel />
 </template>
 
 <style scoped></style>
--- src/components/carousel/BasicCarousel.vue
+++ src/components/carousel/BasicCarousel.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <div><button type="button">Normal Button</button></div>
 </template>
 
 <style scoped></style>
