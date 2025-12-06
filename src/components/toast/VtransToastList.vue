db3d2fd546aae5aff2a7107a688864c3da7aed9b
Ittipat Pattum <nampu@finema.co>
2025-12-06 23:08:20 +0700

    save

--- src/components/toast/VtransToastList.vue
+++ src/components/toast/VtransToastList.vue
@@ -18,7 +18,7 @@ function close(index: number): void {
   <div>
     <button title="add-btn" type="button" class="fixed left-0 bottom-0 m-10 z-10 bg-blue-500 px-8 py-4 rounded-full text-white font-medium text-2xl cursor-pointer shadow-lg" v-on:click="add()">Add Toast</button>
     <div class="fixed top-0 right-0 bottom-0 overflow-hidden au flex flex-col items-center py-4 px-5">
-      <TransitionGroup tag="VtransToastComp"> <VtransToastComp v-for="(item, index) in monthList" v-bind:key="item" v-bind:label="item" v-bind:index="index" v-on:closed="(index: number)=>close(index)" class="my-2" /></TransitionGroup>
+      <TransitionGroup tag="div"> <VtransToastComp v-for="(item, index) in monthList" v-bind:key="item" v-bind:label="item" v-bind:index="index" v-on:closed="(index: number)=>close(index)" class="my-2" /></TransitionGroup>
     </div>
   </div>
 </template>
